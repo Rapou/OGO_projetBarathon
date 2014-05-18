1d2734921238fa855127f94cbba71380a2cea398
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-05-18 19:19:33 +0200

    Ajout de la page contact et modif de script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,13 +1,22 @@
 var bootstrap = "bootstrap.php";
 var app = angular.module('MonApp', ['ngRoute']);
 
+    /*
+     * REGLES DE ROUTAGE DES PAGES
+     */
     app.config(function($routeProvider) {
         $routeProvider
                 .when('/', {templateUrl: 'views/home.html', controller: 'HomeCtrl'})
                 .when('/bars', {templateUrl: 'views/bars.html'})
+                .when('/contact', {templateUrl: 'views/contact.html'})
                 .otherwise({redirectTo: '/'});
     });
 
+    /**
+     * Permet d'afficher la carte dans un layer OpenLayer
+     * @param {type} param1
+     * @param {type} param2
+     */
     app.controller('HomeCtrl', function() {
         map = new OpenLayers.Map('map', {
             projection: new OpenLayers.Projection("EPSG:3857"),
@@ -29,4 +38,9 @@ var app = angular.module('MonApp', ['ngRoute']);
             var nouvelleHauteur = $(window).height() - 40;
             $("#map").css("height", nouvelleHauteur + "px");
         });
+        
+    /*
+     * 
+     */
+
     });
