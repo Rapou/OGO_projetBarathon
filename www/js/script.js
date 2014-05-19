22973b7a03066590e4f7d61f0be6d19541a27b10
Rapou <raphael.schmutz@heig-vd.ch>
2014-05-19 13:12:57 +0200

    Changement Design

--- www/js/script.js
+++ www/js/script.js
@@ -9,8 +9,7 @@ var app = angular.module('Barathon', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
     .when('/', {
-	templateUrl: 'views/home.html', 
-	controller: 'HomeCtrl'
+	templateUrl: 'views/home.html' 
     })
     .when('/bars', {
 	templateUrl: 'views/bars.html'
@@ -22,6 +21,9 @@ app.config(function($routeProvider) {
     .when('/login', {
 	templateUrl: 'views/login.html'
     })
+    .when('/nouvel-utilisateur', {
+	templateUrl: 'views/user-nouveau.html'
+    })
     .otherwise({
 	redirectTo: '/'
     });
@@ -32,44 +34,6 @@ app.config(function($routeProvider) {
 /**
   * Controlleur de la page d'accueil
   */
-app.controller('HomeCtrl', function() {
-    map = new OpenLayers.Map('map', {
-	projection: new OpenLayers.Projection("EPSG:3857"),
-	maxExtent: new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508)
-    });
-    goog = new OpenLayers.Layer.Google("Google layer",
-    {
-	type: google.maps.MapTypeId.ROADMAP,
-	sphericalMercator: true
-    }
-    );
-
-   /* var hauteur = $(document).height() - 40;
-    $("#map").css("height", hauteur + "px");*/
-    map.addLayer(goog);
-    map.setCenter(new OpenLayers.LonLat(8, 47).transform("EPSG:4326", "EPSG:900913"), 6);
-
-/*
-    $(window).resize(function() {
-	var nouvelleHauteur = $(window).height() - 40;
-	$("#map").css("height", nouvelleHauteur + "px");
-    });
-*/
-    
-/*
-    vectorLyr = new OpenLayers.Layer.Vector("Vector layer from GeoJSON", {
-	protocol: new OpenLayers.Protocol.HTTP({
-	    url: bootstrap + "?controller=Bars&action=rends",
-	    format: new OpenLayers.Format.GeoJSON({
-		ignoreExtraDims: true
-	    })
-	}),
-	strategies: [new OpenLayers.Strategy.Fixed()],
-	projection: new OpenLayers.Projection("EPSG:4326")
-    });
-    console.log(vectorLyr);
-    map.addLayer(vectorLyr);*/
-});
 
 /**
   * Controlleur de la page de carte
@@ -82,27 +46,18 @@ app.controller('CarteCtrl', function() {
     goog = new OpenLayers.Layer.Google("Google layer",
     {
 	type: google.maps.MapTypeId.ROADMAP,
-	sphericalMercator: true
+	sphericalMercator: true,
+	maxZoomLevel:20
     }
     );
 
-   /* 
-    var hauteur = $(document).height() - 75;
-    $("#map").css("height", hauteur + "px");
-    */
     map.addLayer(goog);
     map.setCenter(new OpenLayers.LonLat(6.645, 46.53).transform("EPSG:4326", "EPSG:900913"), 14);
-
-/*
-    $(window).resize(function() {
-	var nouvelleHauteur = $(window).height() - 75;
-	$("#map").css("height", nouvelleHauteur + "px");
-    });*/
     
     ptSymbolizer = new OpenLayers.Symbolizer.Point({
-	externalGraphic: "../img/biereLogo.png",
-	graphicWidth: 25,
-	graphicHeight: 25,
+	externalGraphic: "../img/logo_dot.png",
+	graphicWidth: 40,
+	graphicHeight: 50,
 	graphicOpacity: 1,
 	label: "${name}",
 	labelYOffset: -30,
