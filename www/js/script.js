5b04ed4823ffb151708b1b37f65f84d971ca43db
Rapou <raphael.schmutz@heig-vd.ch>
2014-05-19 13:23:32 +0200

    correction mineur

--- www/js/script.js
+++ www/js/script.js
@@ -54,7 +54,7 @@ app.controller('CarteCtrl', function() {
     map.setCenter(new OpenLayers.LonLat(6.645, 46.53).transform("EPSG:4326", "EPSG:900913"), 14);
     
     ptSymbolizer = new OpenLayers.Symbolizer.Point({
-	externalGraphic: "../img/logo_dot.png",
+	externalGraphic: "img/logo_dot.png",
 	graphicWidth: 40,
 	graphicHeight: 50,
 	graphicOpacity: 1,
