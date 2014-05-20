3f28a639b75a8c0434637cadbeac0224d9ca3451
lfrancelet <lauric.francelet@heig-vd.ch>
2014-05-20 21:02:54 +0200

    remove trash

--- www/js/script.js
+++ www/js/script.js
@@ -487,40 +487,6 @@ app.controller('BarathonsCtrl', function($scope, Barathon){
         history.back();
     });
     
-    /*
-    //Récupère la liste des Barathons proposés
-    $scope.barathonsProposes = $http({
-        method: 'GET',
-        url: 'bootstrap.php?controller=Barathons',
-        params: "action=rendBarathonsProposes"
-        }).success(function(data){
-        // With the data succesfully returned, call our callback
-        //callbackFunc(data);
-        alert("Success !");
-        alert("Data : "+data);
-    }).error(function(){
-        alert("error");
-    });
-    
-    //Récupère la liste des Barathons du user loggé (s'il y a un user loggé)
-    if (loggedUserId !== undefined){
-        alert("User logged : " + loggedUserId);
-        
-        $scope.mesBarathons = $http({
-            method: 'GET',
-            url: 'bootstrap.php?controller=Barathons',
-            params: "action=rendMesBarathons&userId="+loggedUserId
-         }).success(function(data){
-            // With the data succesfully returned, call our callback
-            //callbackFunc(data);
-            alert("Success ! : action=rend&userId="+loggedUserId);
-        }).error(function(){
-            alert("error");
-        });
-        
-    }
-     */
-    
 });
 
 /**
