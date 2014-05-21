4044fbcc647ed116ce366d1477ceb3eb7461a143
Rapou <raphael.schmutz@heig-vd.ch>
2014-05-21 15:00:42 +0200

    Mise en place du créateur barathon

--- www/js/script.js
+++ www/js/script.js
@@ -778,39 +778,87 @@ app.controller('BarathonCtrl', function($scope, $routeParams, Barathon, Bar){
 /**
  * Controleur Creation Barathon
  */     
-app.controller('CreationBarathonCtrl', function($scope, $routeParams, Barathon){
-    console.log("Whooot?" + $scope.bars)
+app.controller('CreationBarathonCtrl', function($scope, $routeParams, Barathon, Bar){
     
     $("#creerNewBarathonBtn").click(function() {
         window.location.href= "#validerBarathon";
     });
     
-    /*
-    $scope.bars.events.register("featureselected", $scope.bars, onFeatureSelect);
-    $scope.bars.events.register("featureunselected", $scope.bars, onFeatureUnselect);
-    */
-
-    function onFeatureSelect(evt) {
-	feature = evt.feature;
-	console.log("Select" + feature);
-    }
+    $scope.bars = Bar.find().then(function(bars){
+	$scope.bars = bars;
+	geoBars  = new OpenLayers.Layer.Vector("Bars", {
+	    strategies: [
+		new OpenLayers.Strategy.AnimatedCluster({
+		    distance: 45,
+		    animationMethod: OpenLayers.Easing.Expo.easeOut,
+		    animationDuration: 10
+		})
+	    ],
+	    styleMap: new OpenLayers.StyleMap({
+		"default": new OpenLayers.Style(ptsBar, {
+		    context: ctxBar
+		}),
+		"select": new OpenLayers.Style(ptsBarOver, {
+		    context: ctxBarOver
+		})
+	    })
+	});
+	map.addLayer(geoBars);
+	var features = new Array();
+	
+	$.each($scope.bars.features, function(i, elem){
+	    ptGeom = new OpenLayers.Geometry.Point(elem.geometry.coordinates[0], elem.geometry.coordinates[1]);
+	    ptGeom = ptGeom.transform("EPSG:4326", "EPSG:900913");
+	    features[i] = new OpenLayers.Feature.Vector(ptGeom);
+	    features[i].attributes = {
+		name: elem.properties.name
+	    };
+	});
+	geoBars.addFeatures(features);
+	selectControl = new OpenLayers.Control.SelectFeature(geoBars, {
+	    clickout: false, 
+	    toggle: true,
+	    multiple: true, 
+	    hover: false
+	});
+	map.addControl(selectControl);
+	selectControl.activate();
+	geoBars.events.register("featureselected", features, onFeatureSelect);
+	geoBars.events.register("featureunselected", features, onFeatureUnselect);
+	
+	function onFeatureSelect(evt) {
+	    feature = evt.feature;
+	    if(feature.attributes.count>=2){
+		console.log(feature);
+		map.zoomIn();
+		console.log(feature.geometry.x + "," + feature.geometry.y);
+		map.setCenter(new OpenLayers.LonLat(feature.geometry.x, feature.geometry.y)); 
+		
+	    }else{
+		
+	    }
+	}
               
-    function onFeatureUnselect(evt) {
-	feature = evt.feature;
-	console.log("UnSelect" + feature);
-
-    }
+	function onFeatureUnselect(evt) {
+	    feature = evt.feature;
+	    console.log("UnSelect" + feature);
+	}
     
-    $scope.listeBarsAValider = [
+	
+    }, function(msg){
+	alert(msg);
+    });
+    
+    /*$scope.listeBarsAValider = [
     {
 	"nom" : "Great Escape"
     },
     {
 	"nom" : "Lapin vert"
     },
-    ];
+    ];*/
     
-    console.log("liste " + $scope.listeBarsAValider)
+    // console.log("liste " + $scope.listeBarsAValider)
      
     
 });
