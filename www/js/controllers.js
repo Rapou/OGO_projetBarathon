7a3024f735a9675bdacfe5809bf233a7bc5e80a0
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-05-22 11:34:01 +0200

    Resize noms barathon trop gds+bouton créer Barath

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -451,10 +451,34 @@ app.controller('BarathonCtrl', function($scope, $routeParams, Barathon, Bar){
         window.location.replace("#partieEnCours/" + idPartieEnCours);
     }); 
     
-    
     $(".logo").click(function() {
 	history.back();
     });
+    
+    // Resize de la police quand le nom du barathon est trop long
+    var $body = $('body'); //Cache this for performance
+
+    var setBodyScale = function() {
+        var scaleSource = $body.width(),
+            scaleFactor = 0.35,                     
+            maxScale = 250,
+            minScale = 30; //Tweak these values to taste
+
+        var fontSize = scaleSource * scaleFactor; //Multiply the width of the body by the scaling factor:
+
+        if (fontSize > maxScale) fontSize = maxScale;
+        if (fontSize < minScale) fontSize = minScale; //Enforce the minimum and maximums
+
+        $('.headerText h3').css('font-size', fontSize + '%');
+    }
+
+    $(window).resize(function(){
+        setBodyScale();
+    });
+
+    //Fire it when the page first loads:
+    setBodyScale();
+    // Fin du resize de la police
 });
 
 /**
